8bdf0ac599de89fbe5b502ca130de812261dfc15
elduspogi <alaizaaldus@gmail.com>
2024-12-25 13:06:50 +0800

    Init SnowJs on vue template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script>
+import initSnowJs from './snow';
+
 console.log("Made using Vue with ❤️");
+
+export default {
+  mounted() {
+    initSnowJs();
+  },
+  // unmounted() {
+  //   initSnowJs();
+  // }
+}
 </script>
 
 <template>
